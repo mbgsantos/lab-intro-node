f302b0f2ba571af5c1f16de7ebec92b5c55b58f0
Marcelo Santos <marcelo.b.g.santos@gmail.com>
2023-04-01 14:45:25 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
 
-  max() {}
+    // this.length = this.items.getValue(pos);
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max.apply(null, this.items);
+    }
 
-  sum() {}
+    // return Math.max.apply(null, this.items);
+  }
 
-  avg() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min.apply(null, this.items);
+    }
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => a + b);
+    }
+    // this.items.reduce((a, b) => a + b);
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items.reduce((a, b) => a + b) / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
